f38d920cd697fdb1521fa6945d99d4f879fa35cc
gashino <agaggino@uade.edu.ar>
2024-05-31 17:29:20 -0300

    import fix

--- app/(app)/(tabs)/_layout.jsx
+++ app/(app)/(tabs)/_layout.jsx
@@ -1,14 +1,12 @@
-import { Stack, Tabs } from 'expo-router';
-import React from 'react';
-import { View, Text } from 'react-native';
-
+import { Tabs } from "expo-router";
+import React from "react";
 const AppTabs = () => {
   return (
-    <Tabs screenOptions={{headerShown:true}}>
-      <Tabs.Screen name='inicio'></Tabs.Screen>
-      <Tabs.Screen name='reclamos'></Tabs.Screen>
-      <Tabs.Screen name='denuncias'></Tabs.Screen>
-      <Tabs.Screen name='perfil'></Tabs.Screen>
+    <Tabs screenOptions={{ headerShown: true }}>
+      <Tabs.Screen name="inicio"></Tabs.Screen>
+      <Tabs.Screen name="reclamos"></Tabs.Screen>
+      <Tabs.Screen name="denuncias"></Tabs.Screen>
+      <Tabs.Screen name="perfil"></Tabs.Screen>
     </Tabs>
   );
 };
