2ef74df4cf3123727ebcf0d7d49cd0218a95d8cf
gashino <agaggino@uade.edu.ar>
2024-05-31 22:43:03 -0300

    se agrega boton interactivo para crear cosas

---
+++ app/(app)/(tabs)/reclamoCreation.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const ReclamoCreationPage = () => {
+  return (
+    <View>
+      <Text>ReclamoCreationPage</Text>
+    </View>
+  );
+};
+
+export default ReclamoCreationPage;
