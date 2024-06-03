7bb49e74ab2e559ab696db0e5414f7ab2ae95441
gashino <agaggino@uade.edu.ar>
2024-06-03 02:26:13 -0300

    reclamos view

--- app/(app)/(tabs)/reclamos.jsx
+++ app/(app)/(tabs)/reclamos.jsx
@@ -1,12 +1,54 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useCallback, useEffect, useState } from "react";
+import { View, StyleSheet, ScrollView, ActivityIndicator } from "react-native";
+import CardReclamo from "../../../components/ReclamoCard";
+import { getReclamos } from "../../../services/reclamosService";
+import { useFocusEffect } from "expo-router";
 
 const ReclamosPage = () => {
+  const [reclamos, setReclamos] = useState([]);
+  const [isLoading, setisLoading] = useState(true);
+
+  useFocusEffect(
+    useCallback(() => {
+      setisLoading(true);
+      getReclamos().then((data) => {
+        let reclamosArray = data.map((reclamo) => {
+          return {
+            id: reclamo.idClaim,
+            estado: reclamo.status,
+            creador: reclamo.user.email,
+            responsable: reclamo.employee,
+            descripcion: reclamo.description,
+          };
+        });
+        setReclamos(reclamosArray);
+        setisLoading(false);
+      });
+    }, [])
+  );
+
   return (
-    <View>
-      <Text>ReclamosPage</Text>
+    <View style={styles.container}>
+      <ScrollView style={{ width: "100%", flex: 1 }}>
+        {isLoading ? (
+          <ActivityIndicator />
+        ) : (
+          reclamos.map((reclamo) => (
+            <CardReclamo key={reclamo.id} reclamo={reclamo} />
+          ))
+        )}
+      </ScrollView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 25,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 export default ReclamosPage;
---
+++ components/ReclamoCard.jsx
@@ -0,0 +1,102 @@
+import { Divider } from "@rneui/base";
+import { Chip } from "@rneui/themed";
+import { Link } from "expo-router";
+import React from "react";
+import { View, Text, StyleSheet, Pressable, Image } from "react-native";
+
+const img = require("../assets/images/bana.png");
+
+export default CardReclamo = ({ reclamo }) => {
+  return (
+    <Pressable>
+      <View style={styles.container}>
+        <View style={styles.card}>
+          <View style={{ flexDirection: "row" }}>
+            <Image source={img} style={styles.image}></Image>
+            <View style={styles.details}>
+              <View style={styles.headerMain}>
+                <Text style={styles.headerText}>Reclamo # {reclamo.id}</Text>
+                <Chip containerStyle={styles.chip} color={"red"}>
+                  {reclamo.estado}
+                </Chip>
+              </View>
+              <View style={styles.descripcion}>
+                <Text>{reclamo.descripcion}</Text>
+              </View>
+              <View style={styles.moreInfo}>
+                <Text style={{ marginTop: 2, color: "purple" }}>
+                  +Información
+                </Text>
+              </View>
+            </View>
+          </View>
+        </View>
+      </View>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { justifyContent: "center", alignItems: "center" },
+  card: {
+    height: 160,
+    width: "90%",
+    backgroundColor: "white",
+    borderRadius: 15,
+    elevation: 10,
+    padding: 8,
+    shadowColor: "#000",
+    shadowOffset: { width: 1, height: 3 },
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  image: {
+    width: "30%",
+    height: 140,
+    borderRadius: 15,
+    borderColor: "#2d2033",
+    borderWidth: 3,
+  },
+  details: {
+    height: "100%",
+    width: "70%",
+    padding: 5,
+    flexDirection: "column",
+  },
+  headerText: {
+    fontSize: 17,
+    fontWeight: "bold",
+    marginTop: 5,
+    marginLeft: 3,
+  },
+  chip: {
+    minWidth: 85,
+  },
+  headerMain: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  divider: {
+    marginTop: 6,
+  },
+  descripcion: {
+    padding: 3,
+    borderColor: "black",
+    borderWidth: 0.5,
+    borderRadius: 10,
+    minHeight: 70,
+    maxHeight: 70,
+    marginTop: 6,
+  },
+  moreInfo: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    height: 20,
+    alignItems: "center",
+    marginTop: 5,
+  },
+});
